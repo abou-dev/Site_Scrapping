7334cf83d68d112979e2f55d1fa91b81c18b2026
bassirou-loum <82762223+bassirou-loum@users.noreply.github.com>
2023-07-18 00:23:23 +0000

    mise en place de la recuperation des donnée json

--- Site_Scrapping/Site_Scrapping/views.py
+++ Site_Scrapping/Site_Scrapping/views.py
@@ -1,10 +1,15 @@
+import json
+
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "index.html")
+    with open(r"C:\Users\bassi\OneDrive\Documents\GitHub\Site_Scrapping\Site_Scrapping\Site_Scrapping\data\jumia.json", 'r') as file:
+        data = json.load(file)
+    return render(request, "index.html", context={'datas' : data})
 
 def electronic(request):
-    return render(request, "electronic.html")
+    return render(request, "electronic.html",)
 
 def jewellery(request):
-    return render(request, "jewellery.html")
+    data = request.GET.get('data', '')
+    return render(request, "jewellery.html",  context={'data': data},)
